de47abe68a04108c368bd02a8583f2c5fcb0d88e
Евгения <eugenebeklemisheva2711@gmail.com>
2019-08-01 05:55:32 -0700

    без bootstrap

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
 
-Vue.use(BootstrapVue)
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+}).$mount('#app')
