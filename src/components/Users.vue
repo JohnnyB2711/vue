4dc2da85088a95485623a55a161944b66b09d910
Евгения <eugenebeklemisheva2711@gmail.com>
2019-08-05 10:40:05 +0300

    Вывод таблицы пользователей

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -2,21 +2,57 @@
     <Table>
         <div class="table">
             <h1>Registered users</h1>
+            <b-table striped hover :fields="fields" :items="items"></b-table>
         </div>
     </Table>
 </template>
 
 <script>
+    import axios from 'axios';
     import Table from "../layouts/Table";
+
     export default {
         name: "users",
         components: {Table},
-        data(){
-            return{
-                title:'Registered users'
+        data() {
+            return {
+                fields: [
+                    {
+                        key: 'name',
+                        label: 'Name'
+                    },
+                    {
+                        key: 'username',
+                        label: 'Username'
+                    },
+                    {
+                        key: 'email',
+                        label: 'Email'
+                    }
+                ],
+                items: [
+                    {email: 40, username: 'Dickerson', name: 'Macdonald'},
+                    {email: 21, username: 'Larsen', name: 'Shaw'},
+                    {email: 89, username: 'Geneva', name: 'Wilson'},
+                    {email: 38, username: 'Jami', name: 'Carney'}
+                ]
             }
+        },
+        methods: {
+            async loadusers() {
+                try {
+                   this.items = (await axios.get("https://trainee.smartru.com/api/application")).data
+
+                } catch (e) {
+
+                }
+            }
+        },
+        created() {
+            this.loadusers()
         }
+
     }
 </script>
 
-<style scoped> </style>
+<style scoped></style>
